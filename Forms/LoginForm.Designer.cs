aa9cf78ad3bd52b6e4249999c261a7f98329cb0b
Daniel Bytchek <74202675+Daniel-Bytchek@users.noreply.github.com>
2021-06-02 23:12:57 +0300

    Add files via upload

---
+++ Forms/LoginForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Version_1._1
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.closeLabel = new System.Windows.Forms.Label();
+            this.authorizationLabel = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.loginButton);
+            this.mainPanel.Controls.Add(this.passwordBox);
+            this.mainPanel.Controls.Add(this.loginBox);
+            this.mainPanel.Controls.Add(this.pictureBox1);
+            this.mainPanel.Controls.Add(this.panel2);
+            this.mainPanel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(377, 416);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(127, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Створити аккаунт";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.Brown;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderColor = System.Drawing.Color.SkyBlue;
+            this.loginButton.FlatAppearance.BorderSize = 2;
+            this.loginButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.loginButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGreen;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(81, 333);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(232, 58);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Увійти";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordBox.Location = new System.Drawing.Point(33, 273);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(323, 45);
+            this.passwordBox.TabIndex = 2;
+            this.passwordBox.Text = "Пароль";
+            this.passwordBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passwordBox.Click += new System.EventHandler(this.textBox2_Click);
+            this.passwordBox.Leave += new System.EventHandler(this.passwordBox_Leave);
+            // 
+            // loginBox
+            // 
+            this.loginBox.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginBox.Location = new System.Drawing.Point(33, 196);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(323, 45);
+            this.loginBox.TabIndex = 1;
+            this.loginBox.Text = "Логін";
+            this.loginBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.loginBox.Click += new System.EventHandler(this.textBox1_Click);
+            this.loginBox.Leave += new System.EventHandler(this.loginBox_Leave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Version_1._1.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(152, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Brown;
+            this.panel2.Controls.Add(this.closeLabel);
+            this.panel2.Controls.Add(this.authorizationLabel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(377, 86);
+            this.panel2.TabIndex = 0;
+            // 
+            // closeLabel
+            // 
+            this.closeLabel.AutoSize = true;
+            this.closeLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeLabel.ForeColor = System.Drawing.Color.Silver;
+            this.closeLabel.Location = new System.Drawing.Point(350, 0);
+            this.closeLabel.Name = "closeLabel";
+            this.closeLabel.Size = new System.Drawing.Size(27, 26);
+            this.closeLabel.TabIndex = 1;
+            this.closeLabel.Text = "X";
+            this.closeLabel.Click += new System.EventHandler(this.closeLabel_Click);
+            this.closeLabel.MouseEnter += new System.EventHandler(this.closeLabel_MouseEnter);
+            this.closeLabel.MouseLeave += new System.EventHandler(this.closeLabel_MouseLeave);
+            // 
+            // authorizationLabel
+            // 
+            this.authorizationLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.authorizationLabel.Font = new System.Drawing.Font("Georgia", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.authorizationLabel.ForeColor = System.Drawing.Color.Silver;
+            this.authorizationLabel.Location = new System.Drawing.Point(0, 0);
+            this.authorizationLabel.Name = "authorizationLabel";
+            this.authorizationLabel.Size = new System.Drawing.Size(377, 86);
+            this.authorizationLabel.TabIndex = 0;
+            this.authorizationLabel.Text = "Авторизація";
+            this.authorizationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.authorizationLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.authorizationLabel_MouseDown);
+            this.authorizationLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.authorizationLabel_MouseMove);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 416);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Shown += new System.EventHandler(this.LoginForm_Shown);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label closeLabel;
+        private System.Windows.Forms.Label authorizationLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Forms/RegisterForm1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version_1._1
+{
+    public partial class RegisterForm1 : Form
+    {
+        public RegisterForm1()
+        {
+            InitializeComponent();
+            this.ActiveControl = null;
+         
+        }
+
+        private void registerButton1_Click(object sender, EventArgs e)
+        {
+            if ((this.fnameBox.Text != "Ім'я") && (this.snameBox.Text != "Прізвище") && (this.tnameBox.Text != "По-батькові") && (this.phoneBox.Text != "Телефон") && (this.emailBox.Text != "Пошта") && (this.fnameBox.Text != "") && (this.snameBox.Text != "") && (this.tnameBox.Text != "") && (this.phoneBox.Text != "") && (this.emailBox.Text != ""))
+            {
+                RegisterForm2 f2 = new RegisterForm2(this.fnameBox.Text, this.snameBox.Text, this.tnameBox.Text, this.phoneBox.Text, this.emailBox.Text);
+                this.Hide();
+                f2.ShowDialog();
+                Application.Exit();
+            }
+            else
+                MessageBox.Show("Введено не всю інформацію");
+        }
+
+        private void fnameBox_Enter(object sender, EventArgs e)
+        {
+            if (fnameBox.Text == "Ім'я")
+                fnameBox.Text = "";
+        }
+
+        private void fnameBox_Leave(object sender, EventArgs e)
+        {
+            if (fnameBox.Text == "")
+                fnameBox.Text = "Ім'я";
+        }
+
+        private void snameBox_Enter(object sender, EventArgs e)
+        {
+            if (snameBox.Text == "Прізвище")
+                snameBox.Text = "";
+        }
+
+        private void snameBox_Leave(object sender, EventArgs e)
+        {
+            if (snameBox.Text == "")
+                snameBox.Text = "Прізвище";
+        }
+
+        private void tnameBox_Enter(object sender, EventArgs e)
+        {
+            if (tnameBox.Text == "По-батькові")
+                tnameBox.Text = "";
+        }
+
+        private void tnameBox_Leave(object sender, EventArgs e)
+        {
+            if (tnameBox.Text == "")
+                tnameBox.Text = "По-батькові";
+        }
+
+        private void phoneBox_Enter(object sender, EventArgs e)
+        {
+            if (phoneBox.Text == "Телефон")
+                phoneBox.Text = "";
+        }
+
+        private void phoneBox_Leave(object sender, EventArgs e)
+        {
+            if (phoneBox.Text == "")
+                phoneBox.Text = "Телефон";
+        }
+
+        private void emailBox_Enter(object sender, EventArgs e)
+        {
+            if (emailBox.Text == "Пошта")
+                emailBox.Text = "";
+        }
+
+        private void emailBox_Leave(object sender, EventArgs e)
+        {
+            if (emailBox.Text == "")
+                emailBox.Text = "Пошта";
+        }
+
+        Point lastpoint;
+
+        private void authorizationLabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastpoint.X;
+                this.Top += e.Y - lastpoint.Y;
+            }
+        }
+
+        private void authorizationLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastpoint = new Point(e.X, e.Y);
+        }
+
+        //функции иконки закрытия
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Black;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Silver;
+        }
+
+        private void mainPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+        private void RegisterForm1_Shown(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+        }
+
+    }
+}
